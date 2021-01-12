61b8e4566a905da0711d28628f569c55cbef21c5
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2021-01-11 22:59:21 -0600

    Init

---
+++ src/main/java/com/example/jpa/hibernate/jpa_hibernate_demo/JpaHibernateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa.hibernate.jpa_hibernate_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpa/hibernate/jpa_hibernate_demo/JpaHibernateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa.hibernate.jpa_hibernate_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
